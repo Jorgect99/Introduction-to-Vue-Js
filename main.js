1a2cc81ff7ea99d7d5dadab5772062aa98cc8cc3
Jorgect99 <jorgect99@gmail.com>
2021-02-07 13:34:40 -0700

    First Vue Instance

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello World!",
+        value: 5,
+        img: "https://i.imgur.com/wAELxCA.jpg",
+        link: "https://github.com/Jorgect99"
+    }
+})
