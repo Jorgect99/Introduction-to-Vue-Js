23631cc2ad9e1411db0b474ec670e2df5567fb2b
Jorgect99 <jorgect99@gmail.com>
2021-02-13 12:18:02 -0700

    Conditional, Style Binding, v-for, Computed

---
+++ 4.Class_and_StyleBinding/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        flag: true,
+        styleObject: { backgroundColor: 'green', border: '5px solid red' }
+    },
+    methods: {
+        changeShape(){
+            this.flag = !this.flag;
+            console.log("Holaa")
+        }
+    }
+})
---
+++ 5.List_Rendering_with_v-for/main.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        users: [{
+            id: 1,
+            name: "alice",
+            profession: "developer"
+        },
+        {
+            id: 2,
+            name: "bob",
+            profession: "developer"
+        },
+        {
+            id: 3,
+            name: "Batman",
+            profession: "developer"
+        },
+        {
+            id: 4,
+            name: "Robin",
+            profession: "developer"
+        },
+        {
+            id: 5,
+            name: "SuperMan",
+            profession: "developer"
+        }]
+    },
+    
+})
